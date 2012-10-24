a0c6754ff7aeca544b75c4bb4944368ada9c6e6a
Greg Allen <me@jga.me>
2012-10-24 14:11:03 -0700

    built

--- dist/notice.js
+++ dist/notice.js
@@ -1,10 +1,10 @@
 /*!
-  * notice - a jQuery notification plugin 
-  * v0.2.0
-  * https://github.com/jgallen23/notice
-  * copyright JGA 2012
-  * MIT License
-  */
+ * notice - a jQuery notification plugin
+ * v0.2.0
+ * https://github.com/jgallen23/notice
+ * copyright JGA 2012
+ * MIT License
+*/
 
 (function($) {
   var timeout;
