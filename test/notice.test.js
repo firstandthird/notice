f12ffa51482a53e07168a61e5bedf699e5d47c4c
Troy Whiteley <troy@somanyscientists.com>
2013-11-23 13:07:39 -0800

    A good start on tests.
    Fixes #10

--- test/notice.test.js
+++ test/notice.test.js
@@ -1,4 +1,42 @@
-
 suite('notice', function() {
+  teardown(function() {
+    $('.notice').remove();
+  });
 
+  suite('init', function() {
+    test('should have notice method', function() {
+      assert.ok(typeof $.notice === 'function');
+    });
+  });
+
+  suite('display', function() {
+    setup(function() {
+      $.notice('test notice');
+    });
+    test('notice container should exist', function() {
+      assert.equal($('.notice').length, 1);
+    });
+
+    test('notice should contain passed text', function() {
+      assert.equal($('.notice .notice-text').text(), 'test notice');
+    });
+  });
+
+  suite('levels', function() {
+    var levels = $.notice.defaults.levels;
+
+    function testLevel(level) {
+      test('level: ' + level, function() {
+        $.notice('test notice', {
+          level: level
+        });
+
+        assert.ok($('.notice').hasClass('notice-'+level));
+      });
+    }
+
+    for(var level in levels) {
+      testLevel(level);
+    }
+  });
 });
