d370235b4f03149a0b89b165f8c146606ceb7e2a
Troy Whiteley <troy@somany.us>
2014-04-22 17:16:49 -0700

    Updated build scripts.
    Fixes #13

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,4 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('script-dist', ['concat:dist', 'uglify:dist']);
-  grunt.registerTask('script-full', ['concat:full', 'uglify:full']);
-  grunt.registerTask('scripts', ['jshint', 'bower', 'script-dist', 'clean:bower', 'bytesize', 'notify:generic']);
-  grunt.registerTask('default', ['scripts']);
-  grunt.registerTask('dev', ['default', 'connect:server', 'notify:watch', 'watch']);
 };
